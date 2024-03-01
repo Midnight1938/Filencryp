d124b6374cd626f8b803d4be95b32c4b148f4927
skshm <eminate894567@protonmail.com>
2024-03-01 22:01:04 +0530

    fix broken

--- src/write.rs
+++ src/write.rs
@@ -1,29 +1,28 @@
-use std::io::{BufWriter, Write, Result};
+use std::fs::File;
+use std::io::{self, BufWriter, ErrorKind, Result, Write};
 use crossbeam::channel::Receiver;
 
 pub fn write_loop(outfile: &str, write_rx: Receiver<Vec<u8>>) -> Result<()> {
     let mut writer: Box<dyn Write> = if !outfile.is_empty() {
-        Box::new(BufWriter::new(std::fs::File::open(outfile)?))
+        Box::new(BufWriter::new(File::create(outfile)?))
     } else {
-        Box::new(BufWriter::new(std::io::stdout()))
+        Box::new(BufWriter::new(io::stdout())) // If the outfile is empty, write to stdout
     };
 
     loop {
-        match write_rx.recv() {
-            Ok(buffer) => {
-                if buffer.is_empty() {
-                    break;
-                }
-                if let Err(e) = writer.write_all(&buffer) {
-                    if e.kind() == std::io::ErrorKind::BrokenPipe {
-                        return Ok(());
-                    }
-                    return Err(e);
-                }
-            },
-            Err(_) => break,
-        }
+        // DONE Recieve Vector of bytes from stats thread
+        let buffer = write_rx.recv().unwrap();
+        if buffer.is_empty() {
+            break;
+        };
+
+        if let Err(e) = writer.write_all(&buffer) {
+            if e.kind() == ErrorKind::BrokenPipe {
+                return Ok(()); // stop cleanly
+            }
+            return Err(e);
+        };
     }
 
-    Ok(())
+    Ok(()) // Keep the loop going
 }
