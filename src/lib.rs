1f1e00e7f9b840b6bfeb4e22ee3c0db2cd693cd4
skshm <eminate894567@protonmail.com>
2024-03-02 18:16:56 +0530

    chunk change

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,4 @@ pub mod read; // For Reading
 pub mod stats;
 pub mod write; // For Writing // For Logging
 
-const CHUNK_SIZE: usize = 16 * 1024; // A pre defined chunk size of 16KB
+const CHUNK_SIZE: usize = 16 * 2048; // A pre defined chunk size
