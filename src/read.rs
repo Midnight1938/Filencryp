be99b9ffe1842c459d80e09fd896ef0c20921a1d
skshm <eminate894567@protonmail.com>
2024-03-02 16:36:00 +0530

    updating for diagnosis

--- src/read.rs
+++ src/read.rs
@@ -17,13 +17,6 @@ pub fn read_loop(
     };
     let mut buffer = [0; CHUNK_SIZE];
 
-    // Reuse key and nonce bytes
-    let mut nonce_bytes = [0; 12];
-    OsRng.fill_bytes(&mut nonce_bytes);
-    let key = load_or_generate_key(key_file, infile);
-    let cipher = Aes256Gcm::new(&key);
-            let nonce = Nonce::from_slice(&nonce_bytes);
-
     loop {
         let num_read = match reader.read(&mut buffer) {
             Ok(0) => break,
@@ -33,18 +26,7 @@ pub fn read_loop(
         let _ = stats_tx.send(num_read); // Don't care if it can't send stats
 
         // TODO Decryption
-        let result = if fs::metadata(key_file).is_ok() {
-            cipher.decrypt(nonce, &buffer[..num_read]).unwrap().to_vec()
-        } else {
-            let ciphertext = cipher.encrypt(nonce, &buffer[..num_read]).unwrap();
-
-            // Include the nonce in the encrypted data
-            let mut result = nonce_bytes.to_vec();
-            result.extend_from_slice(&ciphertext);
-            result
-        };
-
-        if write_tx.send(result).is_err() {
+        if write_tx.send().is_err() {
             break;
         }
     }
@@ -52,22 +34,4 @@ pub fn read_loop(
     let _ = write_tx.send(Vec::new()); // empty vec
 
     Ok(())
-}
-
-// TODO Pull in a file properly
-fn load_or_generate_key(key_file: &str, infile: &str) -> Key<Aes256Gcm> {
-    if fs::metadata(key_file).is_ok() {
-        // Key file exists, load the key
-        let mut key_file = BufReader::new(File::open(key_file).unwrap());
-        let mut key = Vec::new();
-        key_file.read_to_end(&mut key).unwrap();
-        *Key::<Aes256Gcm>::from_slice(key.as_slice())
-    } else {
-        // Key file doesn't exist, generate a new key and save it
-        let key = Aes256Gcm::generate_key(&mut OsRng);
-        let mut key_file = File::create(format!("{}.key", infile.replace("\"", ""))).unwrap();
-
-        key_file.write_all(&key).unwrap();
-        key
-    }
 }
